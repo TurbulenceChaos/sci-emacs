6a72aa6d6ee08c3bd8a8b6457c0a49650b44ec21
TurbulenceChaos <turbulencechaos@gmail.com>
2025-04-24 22:53:16 +0800

    Update init-theme.el

--- lisp/init-theme.el
+++ lisp/init-theme.el
@@ -5,8 +5,8 @@
 (package-install 'leuven-theme)
 (load-theme 'leuven :no-confirm)
 
-(custom-set-faces
- '(line-number-current-line ((t (:inherit line-number :foreground "black")))))
+;; (custom-set-faces
+;;  '(line-number-current-line ((t (:inherit line-number :background "blue" :foreground "white" :weight bold)))))
 
 (provide 'init-theme)
 ;;; init-theme.el ends here
