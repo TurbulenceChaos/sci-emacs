68268bec64db7c21bd37c429c803c2d3a9ba1e5c
TurbulenceChaos <turbulencechaos@gmail.com>
2025-04-26 23:12:05 +0800

    Update init-dashboard.el

--- lisp/init-dashboard.el
+++ lisp/init-dashboard.el
@@ -6,16 +6,19 @@
   (package-install 'dashboard))
 (require 'dashboard)
 
-(setq dashboard-banner-logo-title "Welcome to Sci-Emacs!")
-(setq dashboard-center-content t)
+(setq dashboard-banner-logo-title "https://github.com/TurbulenceChaos/Sci-Emacs")
+(setq dashboard-footer-messages '("Welcome to Sci-Emacs!"))
 (setq dashboard-items '((recents . 5)
+                        (bookmarks . 5)
                         (projects . 5)))
 (setq dashboard-display-icons-p t)
 (setq dashboard-icon-type 'nerd-icons)
 (setq dashboard-set-heading-icons t)
 (setq dashboard-set-file-icons t)
 (dashboard-modify-heading-icons '((recents . "nf-oct-file")
+				  (bookmarks . "nf-oct-book")
                                   (projects . "nf-oct-rocket")))
+(setq dashboard-center-content t)
 (dashboard-setup-startup-hook)
 
 
