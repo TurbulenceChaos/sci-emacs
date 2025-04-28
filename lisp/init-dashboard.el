d026bcd8778eef6459e99a157d69c6871d7398fa
TurbulenceChaos <turbulencechaos@gmail.com>
2025-04-28 11:21:40 +0800

    Modify `org-sliced-images-display-inline-images` for small images

--- lisp/init-dashboard.el
+++ lisp/init-dashboard.el
@@ -6,14 +6,14 @@
       dashboard-footer-messages '("https://github.com/TurbulenceChaos/Sci-Emacs")
       dashboard-startup-banner (expand-file-name "Sci-Emacs.png" user-emacs-directory)
       dashboard-center-content t
+      dashboard-projects-backend 'project-el
       dashboard-items '((recents . 5)
 			(bookmarks . 5)
 			(projects . 5))
       dashboard-icon-type 'nerd-icons
       dashboard-display-icons-p t
       dashboard-set-heading-icons t
-      dashboard-set-file-icons t
-      dashboard-projects-backend 'project-el)
+      dashboard-set-file-icons t)
 
 (unless (package-installed-p 'dashboard)
   (package-install 'dashboard))
--- lisp/init-org.el
+++ lisp/init-org.el
@@ -134,6 +134,159 @@
 
 (setq org-sliced-images-round-image-height t)
 
+(with-eval-after-load 'org-sliced-images
+  (defun org-sliced-images-display-inline-images (&optional include-linked refresh beg end)
+    "Display sliced inline images.
+
+See the docstring of `org-display-inline-images' for more info. This advice is
+an amalgamation of different versions of that function, with the goal of being
+compatible with Emacs 28+."
+    (interactive "P")
+    (when (display-graphic-p)
+      (when refresh
+	(org-sliced-images-remove-inline-images beg end)
+	(when (fboundp 'clear-image-cache) (clear-image-cache)))
+      (let ((end (or end (point-max))))
+	(org-with-point-at (or beg (point-min))
+          (let* ((case-fold-search t)
+		 (file-extension-re (image-file-name-regexp))
+		 (link-abbrevs (mapcar #'car
+                                       (append org-link-abbrev-alist-local
+                                               org-link-abbrev-alist)))
+		 ;; Check absolute, relative file names and explicit
+		 ;; "file:" links.  Also check link abbreviations since
+		 ;; some might expand to "file" links.
+		 (file-types-re
+                  (format "\\[\\[\\(?:file%s:\\|attachment:\\|[./~]\\)\\|\\]\\[\\(<?\\(?:file\\|attachment\\):\\)"
+                          (if (not link-abbrevs) ""
+                            (concat "\\|" (regexp-opt link-abbrevs))))))
+            (while (re-search-forward file-types-re end t)
+              (let* ((link (org-element-lineage
+                            (save-match-data (org-element-context))
+                            '(link) t))
+                     (linktype (org-element-property :type link))
+                     (inner-start (match-beginning 1))
+                     (path
+                      (cond
+                       ;; No link at point; no inline image.
+                       ((not link) nil)
+                       ;; File link without a description.  Also handle
+                       ;; INCLUDE-LINKED here since it should have
+                       ;; precedence over the next case.  I.e., if link
+                       ;; contains filenames in both the path and the
+                       ;; description, prioritize the path only when
+                       ;; INCLUDE-LINKED is non-nil.
+                       ((or (not (org-element-property :contents-begin link))
+                            include-linked)
+			(and (or (equal "file" linktype)
+				 (equal "attachment" linktype))
+                             (org-element-property :path link)))
+                       ;; Link with a description.  Check if description
+                       ;; is a filename.  Even if Org doesn't have syntax
+                       ;; for those -- clickable image -- constructs, fake
+                       ;; them, as in `org-export-insert-image-links'.
+                       ((not inner-start) nil)
+                       (t
+			(org-with-point-at inner-start
+                          (and (looking-at
+				(if (char-equal ?< (char-after inner-start))
+                                    org-link-angle-re
+                                  org-link-plain-re))
+                               ;; File name must fill the whole
+                               ;; description.
+                               (= (org-element-property :contents-end link)
+                                  (match-end 0))
+                               (progn
+				 (setq linktype (match-string 1))
+				 (match-string 2))))))))
+		(when (and path (string-match-p file-extension-re path))
+                  (let ((file (if (equal "attachment" linktype)
+                                  (progn
+                                    (require 'org-attach)
+                                    (ignore-errors (org-attach-expand path)))
+				(expand-file-name path))))
+                    ;; Expand environment variables.
+                    (when file (setq file (substitute-in-file-name file)))
+                    (when (and file (file-exists-p file))
+                      (let ((width (org-display-inline-image--width link))
+                            ;; Support Emacs 30+ align in a sec
+                            ;; (align (and (fboundp 'org-image--align)
+                            ;;             (org-image--align link)))
+                            (old (get-char-property-and-overlay
+                                  (org-element-property :begin link)
+                                  'org-image-overlay)))
+			(if (and (car-safe old) refresh)
+                            (image-flush (overlay-get (cdr old) 'display))
+                          (let ((image (org-sliced-images--create-inline-image file width)))
+                            (when image
+                              (org-sliced-images--without-undo
+                               (let* ((image-pixel-cons (image-size image t))
+                                      (image-pixel-h (cdr image-pixel-cons))
+                                      (font-height (default-font-height)))
+				 ;; Round image height
+				 (when org-sliced-images-round-image-height
+                                   (setq image-pixel-h
+					 (truncate (* (fround (/ image-pixel-h font-height 1.0)) font-height)))
+                                   (setf (image-property image :height) image-pixel-h)
+                                   (setf (image-property image :width) nil))
+				 (let* ((image-line-h (max 1.0001 (/ image-pixel-h font-height 1.0001)))
+					(y 0.0) (dy (/ image-line-h))
+					(left-margin nil)
+					(dummy-zone-start nil)
+					(dummy-zone-end nil)
+					(ovfam nil))
+                                   (image-flush image)
+                                   (org-with-point-at (org-element-property :begin link)
+                                     (when (> (current-column) 0)
+                                       (setq left-margin (current-column)))
+                                     (while (< y 1.0)
+                                       (let (slice-start slice-end)
+					 (if (= y 0.0)
+                                             ;; Overlay link
+                                             (progn
+                                               (setq slice-start (org-element-property :begin link))
+                                               (setq slice-end (org-element-property :end link))
+                                               (end-of-line)
+                                               (delete-char 1)
+                                               (insert (propertize "\n" 'line-height t)))
+                                           (setq slice-start (line-beginning-position))
+                                           (setq slice-end (1+ (line-beginning-position)))
+                                           (if (and org-sliced-images-consume-dummies
+                                                    (equal
+                                                     (buffer-substring-no-properties
+                                                      (line-beginning-position)
+                                                      (line-end-position))
+                                                     " "))
+                                               ;; Consume next line as dummy
+                                               (progn
+						 (when left-margin
+                                                   (put-text-property
+                                                    slice-start
+                                                    slice-end
+                                                    'line-prefix
+                                                    `(space :width ,left-margin)))
+						 (put-text-property
+                                                  (line-end-position) (1+ (line-end-position)) 'line-height t)
+						 (forward-line))
+                                             ;; Create dummy line
+                                             (insert (if left-margin
+							 (propertize " " 'line-prefix `(space :width ,left-margin))
+                                                       " "))
+                                             (insert (propertize "\n" 'line-height t)))
+                                           (when (not dummy-zone-start)
+                                             (setq dummy-zone-start slice-start))
+                                           (setq dummy-zone-end (1+ slice-end)))
+					 (push
+                                          (org-sliced-images--make-inline-image-overlay
+                                           slice-start
+                                           slice-end
+                                           (list (list 'slice 0 y 1.0 dy) image))
+                                          ovfam))
+                                       (setq y (+ y dy))))
+                                   (setq end (+ end (* 2 (- (ceiling image-line-h) 1))))
+                                   (push (make-overlay dummy-zone-start dummy-zone-end) ovfam)
+                                   (push ovfam org-sliced-images--image-overlay-families)))))))))))))))))))
+
 (defun my/org-sliced-images-display-inline-images ()
   "Remove org-sliced-images before displaying it."
   (interactive)
