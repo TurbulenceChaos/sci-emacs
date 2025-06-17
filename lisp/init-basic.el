7342309697781ae057a3fcf813465e562c44dea0
TurbulenceChaos <211110103110@stu.just.edu.cn>
2025-06-17 21:03:40 +0800

    Update sci-wolfram-theme

--- lisp/init-basic.el
+++ lisp/init-basic.el
@@ -3,7 +3,8 @@
 ;;; Code:
 
 (set-face-attribute 'default nil :height 240)
-(load-theme 'sci-wolfram t)
+(when (display-graphic-p)
+  (load-theme 'sci-wolfram t))
 
 (global-hl-line-mode t)
 (add-hook 'prog-mode-hook #'display-line-numbers-mode)
